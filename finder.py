ec41e1319ba047539644562835ee08545558149d
Srajan Soni <sonisrajan96@gmail.com>
2018-08-23 21:42:24 +0530

    Update finder.py

--- finder.py
+++ finder.py
@@ -1,9 +1,4 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
 import sys
-
-# !{sys.executable} -m pip install sendgrid
-
 from apscheduler.schedulers.blocking import BlockingScheduler
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
@@ -13,51 +8,34 @@ import os
 from sendgrid.helpers.mail import *
 today = datetime.now().date()
 
-# print(type(today))
-
-# print(os.environ.get('SENDGRID_API_KEY'))
-# today=now.strftime("%d-%m-%y")
-
-###HELPERS###
-
 batchdict = {
-  'CERED-10': ['chetangsti@gmail.com','srajan1996@gmail.com'], 'CERED-04': ['nikhilpvp6@gmail.com']}
-
+'CERED-10': ['chetangsti@gmail.com','srajan1996@gmail.com'], 'CERED-04': ['nikhilpvp6@gmail.com']}
 
 def sendemail(emaillist, email_content):
     for email in emaillist:
-		sg =sendgrid.SendGridAPIClient(apikey=os.environ.get('SENDGRID_API_KEY'))
-		from_email = Email('iesinformer@srajanlabs.com')
-		to_email = Email(email)
-		subject = 'New Notificatison from IESMASTERS for you'
-		content = Content('text/html', email_content)
-		mail = Mail(from_email, subject, to_email, content)
-		response = sg.client.mail.send.post(request_body=mail.get())
-		print (response.status_code)
-        
-        
-        
+        sg =sendgrid.SendGridAPIClient(apikey=os.environ.get('SENDGRID_API_KEY'))
+        from_email = Email('iesinformer@srajanlabs.com')
+        to_email = Email(email)
+        subject = 'New Notificatison from IESMASTERS for you'
+        content = Content('text/html', email_content)
+        mail = Mail(from_email, subject, to_email, content)
+        response = sg.client.mail.send.post(request_body=mail.get())
+        print (response.status_code)
 
 def addemail(batch, email):
     batchdict[batch].append(email)
-
 
 sched = BlockingScheduler()
 
 @sched.scheduled_job('interval', hours=12)
 def scheduled_job():
-	print("Process started")
-	response = urlopen('https://iesmaster.org/')
-	html = response.read()
-
-    soup = BeautifulSoup(html, 'html5lib')
+    print("Process started")
+    response = urlopen('https://iesmaster.org/')
+    html = response.read()
+    soup=BeautifulSoup(html,'html5lib')
     news = soup.find('div', {'id': 'newser'}).ul
     for child in news:
         if str(type(child)) == "<class 'bs4.element.Tag'>":
-
-    #         print("----------")
-    #         print(child)
-
             url = child.a['href']
             date = datetime.strptime(child.find_all(class_='ns-dt'
                     )[0].contents[0], '%d-%m-%y').date()
@@ -85,20 +63,10 @@ def scheduled_job():
 
                         sendemail(batchdict[batch], content)
 
-    #         print("==========")
+#         print("==========")
 
 ####
 # MAIN PROGRAM
 ####
 
 sched.start()
-
-# scheduled_job()
-
-
-
-        
-
-    
-        
-        
